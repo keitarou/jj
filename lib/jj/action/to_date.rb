15fcbe29d308f5fb3cede7ee17aa7d209f701dac
keitarou <keitarou.oonishi@gmail.com>
2014-07-06 02:50:21 +0900

    change output date format

--- lib/jj/action/to_date.rb
+++ lib/jj/action/to_date.rb
@@ -3,7 +3,7 @@ class JJActionTodate
 
   def execute(timestamp)
     time = Time.at(timestamp.to_i)
-    time.strftime("%Y/%m/%d %H:%M:%S")
+    time.strftime("%Y-%m-%d %H:%M:%S")
   end
 
 end
--- spec/jj_spec.rb
+++ spec/jj_spec.rb
@@ -8,7 +8,7 @@ end
 
 describe "JJActionToDate" do
   it "return date string" do
-    JJ.new.todate(1404572400).should   == '2014/07/06 00:00:00'
-    JJ.new.todate('1404572400').should == '2014/07/06 00:00:00'
+    JJ.new.todate(1404572400).should   == '2014-07-06 00:00:00'
+    JJ.new.todate('1404572400').should == '2014-07-06 00:00:00'
   end
 end
